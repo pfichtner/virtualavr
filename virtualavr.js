6291f4ad415f3f7faa71c48902d3ecb5ab5b876e
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-07-13 22:56:09 +0200

    buffer no more global

--- virtualavr.js
+++ virtualavr.js
@@ -11,7 +11,6 @@ const arduinoPinOnPortB = [ 8,9,10,11,12,13,20,21 ];
 
 
 const args = process.argv.slice(2);
-const buff = [];
 
 const runCode = async (inputFilename, portCallback) => {
 	let fileContent = fs.readFileSync(inputFilename).toString();
@@ -52,13 +51,12 @@ const runCode = async (inputFilename, portCallback) => {
 
 	const usart = new avr8js.AVRUSART(cpu, avr8js.usart0Config, 16e6);
 	usart.onByteTransmit = data => process.stdout.write(String.fromCharCode(data));
-	usart.onRxComplete = () => sendNextChar(usart);
-	// process.stdin.setRawMode(true);
-	// process.stdin.resume();
+	const buff = [];
+	usart.onRxComplete = () => sendNextChar(buff, usart);
 	process.stdin.on('data', data => {
 		const bytes = data.toString();
 		for (let i = 0; i < bytes.length; i++) buff.push(bytes.charCodeAt(i));
-		sendNextChar(usart);
+		sendNextChar(buff, usart);
 	});
 
 	const timer = new avr8js.AVRTimer(cpu, avr8js.timer0Config);
@@ -72,7 +70,7 @@ const runCode = async (inputFilename, portCallback) => {
 
 }
 
-function sendNextChar(usart) {
+function sendNextChar(buff, usart) {
 	const ch = buff.shift();
 	if (ch) {
 		usart.writeByte(ch);
