b6bf01e8f0d572b65f061e6298dfef419d8532cc
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-07-24 21:41:21 +0200

    sysout added

--- demo/java/sketchtest/src/main/java/com/ardulink/sketch/ArdulinkArduinoSimulator.java
+++ demo/java/sketchtest/src/main/java/com/ardulink/sketch/ArdulinkArduinoSimulator.java
@@ -33,6 +33,8 @@ public class ArdulinkArduinoSimulator {
 
 			try {
 				BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+				NumberFormat numberParser = NumberFormat.getNumberInstance();
+				System.out.println("Waiting for command of format <pin>=<value>, e.g. D13=true or A0=512");
 				while (true) {
 					System.out.print("$ ");
 					String input = reader.readLine();
@@ -41,13 +43,14 @@ public class ArdulinkArduinoSimulator {
 					if (split.length != 2) {
 						System.err.println("Unable to split " + input);
 					} else {
-						String pin = split[0];
+						String pin = split[0].trim();
+						String value = split[1].trim();
 						try {
-							Number state = NumberFormat.getNumberInstance().parse(split[1]);
+							Number state = numberParser.parse(value);
 							System.out.println("Setting " + pin + " to " + state);
 							connectionToVirtualAvr.setPinState(pin, state.intValue());
 						} catch (ParseException e) {
-							boolean state = Boolean.parseBoolean(split[1]);
+							boolean state = Boolean.parseBoolean(value);
 							System.out.println("Setting " + pin + " to " + state);
 							connectionToVirtualAvr.setPinState(pin, state);
 						}
