23c2ea68481dc1648202fa47b79932efe52b17d4
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-07-24 15:05:47 +0200

    todos added

--- test/virtualavr.spec.js
+++ test/virtualavr.spec.js
@@ -3,6 +3,15 @@ const waitForExpect = require("wait-for-expect");
 
 jest.setTimeout(10000);
 
+// Tests:
+// - Callback called
+// - WS messages sent: 
+//   - pin states
+//   - TX/RX
+//   - debug-messages?
+// - Does run loop/demo sketch without params?
+
+
 describe('My Test Suite', () => {
 	it('My Test Case', async () => {
 		const mockFunction = jest.fn()
