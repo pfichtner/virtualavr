dba87c8d3a8f287ce92f4b68a9d48b9db4218b74
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-08-06 14:00:14 +0200

    house keeping

--- demo/java/sketchtest/src/test/java/com/github/pfichtner/virtualavr/demo/TrafficLightTest.java
+++ demo/java/sketchtest/src/test/java/com/github/pfichtner/virtualavr/demo/TrafficLightTest.java
@@ -1,11 +1,13 @@
 package com.github.pfichtner.virtualavr.demo;
 
+import static com.github.pfichtner.virtualavr.demo.TrafficLightTest.State.state;
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 import static org.awaitility.Awaitility.await;
 
 import java.io.File;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -19,12 +21,27 @@ import com.github.pfichtner.virtualavr.VirtualAvrContainer;
 @Testcontainers
 class TrafficLightTest {
 
+	static class State {
+
+		private final String led;
+		private final Boolean state;
+
+		public State(String led, Boolean state) {
+			this.led = led;
+			this.state = state;
+		}
+
+		public static State state(String led, Boolean state) {
+			return new State(led, state);
+		}
+	}
+
 	private static final String VALUE_PIN = "A1";
 	private static final String REF_PIN = "A0";
 
-	private static final String RED_LED = "D12";
-	private static final String YELLOW_LED = "D11";
 	private static final String GREEN_LED = "D10";
+	private static final String YELLOW_LED = "D11";
+	private static final String RED_LED = "D12";
 
 	@Container
 	static VirtualAvrContainer<?> virtualavr = new VirtualAvrContainer<>()
@@ -49,29 +66,26 @@ class TrafficLightTest {
 	void valueEqualsRef_GreenLedIsIOn() {
 		int someValue = 1022;
 		avr.pinState(REF_PIN, someValue).pinState(VALUE_PIN, someValue);
-		await().until(() -> statesAre(GREEN_LED, TRUE, YELLOW_LED, FALSE, RED_LED, FALSE));
+		await().until(() -> statesAre(state(GREEN_LED, TRUE), state(YELLOW_LED, FALSE), state(RED_LED, FALSE)));
 	}
 
 	@Test
 	void valueGreaterThenRef_RedLedIsIOn() {
 		int someValue = 1022;
 		avr.pinState(REF_PIN, someValue).pinState(VALUE_PIN, someValue + 1);
-		await().until(() -> statesAre(GREEN_LED, FALSE, YELLOW_LED, FALSE, RED_LED, TRUE));
+		await().until(() -> statesAre(state(GREEN_LED, FALSE), state(YELLOW_LED, FALSE), state(RED_LED, TRUE)));
 	}
 
 	@Test
 	void valueGreaterWithin90Percent_YellowLedIsIOn() {
 		int ref = 100;
 		avr.pinState(REF_PIN, ref).pinState(VALUE_PIN, ref * 90 / 100 + 1);
-		await().until(() -> statesAre(GREEN_LED, FALSE, YELLOW_LED, TRUE, RED_LED, FALSE));
+		await().until(() -> statesAre(state(GREEN_LED, FALSE), state(YELLOW_LED, TRUE), state(RED_LED, FALSE)));
 	}
 
-	private boolean statesAre(String led0, Boolean state0, String led1, Boolean state1, String led2, Boolean state2) {
+	private boolean statesAre(State... states) {
 		Map<String, Object> lastStates = avr.lastStates();
-		return state0.equals(lastStates.getOrDefault(led0, FALSE)) //
-				&& state1.equals(lastStates.getOrDefault(led1, FALSE)) //
-				&& state2.equals(lastStates.getOrDefault(led2, FALSE)) //
-		;
+		return Arrays.stream(states).allMatch(s -> s.state.equals(lastStates.getOrDefault(s.led, FALSE)));
 	}
 
 }
