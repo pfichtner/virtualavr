fad78fc9e1226cd478aa2644f5f48d135046955c
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-08-06 13:15:18 +0200

    cleanup

--- demo/java/sketchtest/src/test/java/com/github/pfichtner/virtualavr/demo/TrafficLightTest.java
+++ demo/java/sketchtest/src/test/java/com/github/pfichtner/virtualavr/demo/TrafficLightTest.java
@@ -1,10 +1,12 @@
 package com.github.pfichtner.virtualavr.demo;
 
+import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 import static org.awaitility.Awaitility.await;
 
 import java.io.File;
 import java.net.URISyntaxException;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -52,21 +54,29 @@ class TrafficLightTest {
 	void valueEqualsRef_GreenLedIsIOn() {
 		int someValue = 1022;
 		avr.pinState(REF_PIN, someValue).pinState(VALUE_PIN, someValue);
-		await().until(() -> TRUE.equals(avr.lastStates().get(GREEN_LED)));
+		await().until(() -> statesAre(GREEN_LED, TRUE, YELLOW_LED, FALSE, RED_LED, FALSE));
 	}
 
 	@Test
 	void valueGreaterThenRef_RedLedIsIOn() {
 		int someValue = 1022;
 		avr.pinState(REF_PIN, someValue).pinState(VALUE_PIN, someValue + 1);
-		await().until(() -> TRUE.equals(avr.lastStates().get(RED_LED)));
+		await().until(() -> statesAre(GREEN_LED, FALSE, YELLOW_LED, FALSE, RED_LED, TRUE));
 	}
 
 	@Test
 	void valueGreaterWithin90Percent_YellowLedIsIOn() {
 		int ref = 100;
 		avr.pinState(REF_PIN, ref).pinState(VALUE_PIN, ref * 90 / 100 + 1);
-		await().until(() -> TRUE.equals(avr.lastStates().get(YELLOW_LED)));
+		await().until(() -> statesAre(GREEN_LED, FALSE, YELLOW_LED, TRUE, RED_LED, FALSE));
+	}
+
+	private boolean statesAre(String led0, Boolean state0, String led1, Boolean state1, String led2, Boolean state2) {
+		Map<String, Object> lastStates = avr.lastStates();
+		return state0.equals(lastStates.getOrDefault(led0, FALSE)) //
+				&& state1.equals(lastStates.getOrDefault(led1, FALSE)) //
+				&& state2.equals(lastStates.getOrDefault(led2, FALSE)) //
+		;
 	}
 
 }
