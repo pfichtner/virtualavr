9ebdae719e1ed1deba95c383757e902677000a12
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-08-15 12:13:14 +0200

    download Ardulink sketch from github

--- demo/java/sketchtest/src/test/java/com/ardulink/sketch/ArdulinkFirmwareTest.java
+++ demo/java/sketchtest/src/test/java/com/ardulink/sketch/ArdulinkFirmwareTest.java
@@ -2,9 +2,17 @@ package com.ardulink.sketch;
 
 import static com.github.pfichtner.virtualavr.SerialConnectionAwait.awaiter;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -14,9 +22,26 @@ import com.github.pfichtner.virtualavr.VirtualAvrContainer;
 @Testcontainers
 class ArdulinkFirmwareTest {
 
+	static final String REMOTE_INO_FILE = "https://raw.githubusercontent.com/Ardulink/Ardulink-2/master/deploy-dist/rootfolder/sketches/ArdulinkProtocol/ArdulinkProtocol.ino";
+
+	static File inoFile;
+
+	@BeforeAll
+	static void loadFromNet(@TempDir File tmpDir) throws MalformedURLException, IOException {
+		inoFile = downloadTo(new URL(REMOTE_INO_FILE), new File(tmpDir, "ArdulinkProtocol.ino"));
+	}
+
+	private static File downloadTo(URL source, File target)
+			throws IOException, MalformedURLException, FileNotFoundException {
+		try (BufferedInputStream in = new BufferedInputStream(source.openStream());
+				FileOutputStream out = new FileOutputStream(target)) {
+			out.write(in.readAllBytes());
+		}
+		return target;
+	}
+
 	@Container
-	VirtualAvrContainer<?> virtualAvrContainer = new VirtualAvrContainer<>()
-			.withSketchFile(new File("/tmp/ArdulinkProtocol.ino"));
+	VirtualAvrContainer<?> virtualAvrContainer = new VirtualAvrContainer<>().withSketchFile(inoFile);
 
 	@Test
 	void canDetectArduinoThatSendInitialMessage() throws Exception {
