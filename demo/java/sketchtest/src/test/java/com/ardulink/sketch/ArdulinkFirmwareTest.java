d9eecd7d1244ba17efa81d37dfb2b7cbb091e882
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-08-22 17:32:39 +0200

    pin switch test added

--- demo/java/sketchtest/src/test/java/com/ardulink/sketch/ArdulinkFirmwareTest.java
+++ demo/java/sketchtest/src/test/java/com/ardulink/sketch/ArdulinkFirmwareTest.java
@@ -1,6 +1,9 @@
 package com.ardulink.sketch;
 
 import static com.github.pfichtner.virtualavr.SerialConnectionAwait.awaiter;
+import static com.github.pfichtner.virtualavr.VirtualAvrConnection.PinReportMode.DIGITAL;
+import static org.awaitility.Awaitility.await;
+import static org.testcontainers.shaded.com.google.common.base.Objects.equal;
 
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -17,6 +20,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.github.pfichtner.virtualavr.SerialConnection;
+import com.github.pfichtner.virtualavr.VirtualAvrConnection;
 import com.github.pfichtner.virtualavr.VirtualAvrContainer;
 
 @Testcontainers
@@ -57,6 +61,34 @@ class ArdulinkFirmwareTest {
 			awaiter(connection).waitReceivedAnything().sendAwait("alp://notn/?id=" + id + "\n",
 					"alp://rply/ok?id=" + id + "\n");
 		}
+	}
+
+	@Test
+	void canSwitchPin() throws Exception {
+		int pin = 12;
+		try (SerialConnection connection = virtualAvrContainer.serialConnection()) {
+			awaiter(connection).waitReceivedAnything();
+			VirtualAvrConnection avr = virtualAvrContainer.avr();
+			avr.pinReportMode("D" + pin, DIGITAL);
+			{
+				boolean state = true;
+				connection.send(powerDigital(pin, state));
+				await().until(() -> stateOfPinIs(avr, "D" + pin, state));
+			}
+			{
+				boolean state = false;
+				connection.send(powerDigital(pin, state));
+				await().until(() -> stateOfPinIs(avr, "D" + pin, state));
+			}
+		}
+	}
+
+	static boolean stateOfPinIs(VirtualAvrConnection avr, String pin, boolean state) {
+		return equal(avr.lastStates().get(pin), state);
+	}
+
+	static String powerDigital(int pin, boolean state) {
+		return "alp://ppsw/" + pin + "/" + (state ? "1" : "0") + "\n";
 	}
 
 }
