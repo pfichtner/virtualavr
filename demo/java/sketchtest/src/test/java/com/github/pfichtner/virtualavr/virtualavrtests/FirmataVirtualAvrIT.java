a05c7f4bdbd956030254b2c92441d038c882e438
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-08-22 22:40:11 +0200

    house keeping

--- demo/java/sketchtest/src/test/java/com/github/pfichtner/virtualavr/virtualavrtests/FirmataVirtualAvrIT.java
+++ demo/java/sketchtest/src/test/java/com/github/pfichtner/virtualavr/virtualavrtests/FirmataVirtualAvrIT.java
@@ -2,16 +2,18 @@ package com.github.pfichtner.virtualavr.virtualavrtests;
 
 import static com.github.pfichtner.virtualavr.SerialConnectionAwait.awaiter;
 import static com.github.pfichtner.virtualavr.demo.TestcontainerSupport.imageName;
-import static com.github.pfichtner.virtualavr.demo.TestcontainerSupport.onlyPullIfEnabled;
 import static com.github.pfichtner.virtualavr.demo.TestcontainerSupport.loadClasspath;
+import static com.github.pfichtner.virtualavr.demo.TestcontainerSupport.onlyPullIfEnabled;
 import static java.lang.Integer.parseInt;
 import static java.lang.Integer.toBinaryString;
 import static java.time.temporal.ChronoUnit.SECONDS;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -42,17 +44,16 @@ class FirmataVirtualAvrIT {
 	static final byte capabilityQuery = 0x6B;
 	static final byte endSysex = (byte) 0xF7;
 
-	byte[] firmwareStartupInfo = new byte[] { -7, 2, 5, -16, 121, 2, 5, 115, 0, 107, 0, 101, 0, 116, 0, 99, 0, 104, 0,
-			46, 0, 105, 0, 110, 0, 111, 0, -9 };
-	byte[] firmwareInfo = new byte[] { -16, 121, 2, 5, 115, 0, 107, 0, 101, 0, 116, 0, 99, 0, 104, 0, 46, 0, 105, 0,
-			110, 0, 111, 0, -9 };
-	byte[] capabilities = new byte[] { -16, 108, 127, 127, 0, 1, 11, 1, 1, 1, 4, 14, 127, 0, 1, 11, 1, 1, 1, 3, 8, 4,
-			14, 127, 0, 1, 11, 1, 1, 1, 4, 14, 127, 0, 1, 11, 1, 1, 1, 3, 8, 4, 14, 127, 0, 1, 11, 1, 1, 1, 3, 8, 4, 14,
-			127, 0, 1, 11, 1, 1, 1, 4, 14, 127, 0, 1, 11, 1, 1, 1, 4, 14, 127, 0, 1, 11, 1, 1, 1, 3, 8, 4, 14, 127, 0,
-			1, 11, 1, 1, 1, 3, 8, 4, 14, 127, 0, 1, 11, 1, 1, 1, 3, 8, 4, 14, 127, 0, 1, 11, 1, 1, 1, 4, 14, 127, 0, 1,
-			11, 1, 1, 1, 4, 14, 127, 0, 1, 11, 1, 1, 1, 2, 10, 4, 14, 127, 0, 1, 11, 1, 1, 1, 2, 10, 4, 14, 127, 0, 1,
-			11, 1, 1, 1, 2, 10, 4, 14, 127, 0, 1, 11, 1, 1, 1, 2, 10, 4, 14, 127, 0, 1, 11, 1, 1, 1, 2, 10, 4, 14, 6, 1,
-			127, 0, 1, 11, 1, 1, 1, 2, 10, 4, 14, 6, 1, 127, -9 };
+	byte[] firmwareInfo = new byte[] { startSysex, 121, 2, 5, 115, 0, 107, 0, 101, 0, 116, 0, 99, 0, 104, 0, 46, 0, 105,
+			0, 110, 0, 111, 0, endSysex };
+	byte[] firmwareStartupInfo = concat(new byte[] { -7, 2, 5 }, firmwareInfo);
+	byte[] capabilities = new byte[] { startSysex, 108, 127, 127, 0, 1, 11, 1, 1, 1, 4, 14, 127, 0, 1, 11, 1, 1, 1, 3,
+			8, 4, 14, 127, 0, 1, 11, 1, 1, 1, 4, 14, 127, 0, 1, 11, 1, 1, 1, 3, 8, 4, 14, 127, 0, 1, 11, 1, 1, 1, 3, 8,
+			4, 14, 127, 0, 1, 11, 1, 1, 1, 4, 14, 127, 0, 1, 11, 1, 1, 1, 4, 14, 127, 0, 1, 11, 1, 1, 1, 3, 8, 4, 14,
+			127, 0, 1, 11, 1, 1, 1, 3, 8, 4, 14, 127, 0, 1, 11, 1, 1, 1, 3, 8, 4, 14, 127, 0, 1, 11, 1, 1, 1, 4, 14,
+			127, 0, 1, 11, 1, 1, 1, 4, 14, 127, 0, 1, 11, 1, 1, 1, 2, 10, 4, 14, 127, 0, 1, 11, 1, 1, 1, 2, 10, 4, 14,
+			127, 0, 1, 11, 1, 1, 1, 2, 10, 4, 14, 127, 0, 1, 11, 1, 1, 1, 2, 10, 4, 14, 127, 0, 1, 11, 1, 1, 1, 2, 10,
+			4, 14, 6, 1, 127, 0, 1, 11, 1, 1, 1, 2, 10, 4, 14, 6, 1, 127, endSysex };
 
 	SerialConnection serialConnection;
 	SerialConnectionAwait awaiter;
@@ -69,19 +70,6 @@ class FirmataVirtualAvrIT {
 		if (bytes.length > 0) {
 			System.out.println(testInfo.getDisplayName() + ", remaining bytes (" + new String(bytes) + "): ");
 			printBytes(bytes);
-		}
-	}
-
-	private void printBytes(byte[] bytes) {
-		for (int i = 0; i < bytes.length; i++) {
-			byte b = bytes[i];
-			System.out.print(String.format("%04d", i));
-			System.out.print(": ");
-			System.out.print(String.format("%08d", parseInt(toBinaryString(b & 0xFF))));
-			System.out.print(" ");
-			System.out.print(String.format("%02x", b));
-			System.out.print(" ");
-			System.out.println(b);
 		}
 	}
 
@@ -102,7 +90,33 @@ class FirmataVirtualAvrIT {
 				.sendAwait(sysex(capabilityQuery), b -> Arrays.equals(b, capabilities));
 	}
 
-	byte[] sysex(byte... message) {
+	static byte[] concat(byte[]... arrays) {
+		return Stream.of(arrays).collect(ByteArrayOutputStream::new, FirmataVirtualAvrIT::write,
+				(baos1, baos2) -> baos1.write(baos2.toByteArray(), 0, baos2.size())).toByteArray();
+	}
+
+	static void write(ByteArrayOutputStream baos, byte[] bytes) {
+		try {
+			baos.write(bytes);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	static void printBytes(byte[] bytes) {
+		for (int i = 0; i < bytes.length; i++) {
+			byte b = bytes[i];
+			System.out.print(String.format("%04d", i));
+			System.out.print(": ");
+			System.out.print(String.format("%08d", parseInt(toBinaryString(b & 0xFF))));
+			System.out.print(" ");
+			System.out.print(String.format("%02x", b));
+			System.out.print(" ");
+			System.out.println(b);
+		}
+	}
+
+	static byte[] sysex(byte... message) {
 		byte[] bytes = new byte[message.length + 2];
 		System.arraycopy(message, 0, bytes, 1, message.length);
 		bytes[0] = startSysex;
