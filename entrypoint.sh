f44ab786cb97460c07bd254fb545aab93491b447
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-11-29 18:38:04 +0100

    migrated from ubuntu to alpine

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ cd /sketch
 FILENAME=${FILENAME:-'sketch.ino'}
 BAUDRATE=${BAUDRATE:-9600}
 
-socat ${VERBOSITY:-} pty,rawer,link=${VIRTUALDEVICE:-'/dev/virtualavr0'},group-late=${DEVICEGROUP:-'dialout'},mode=${DEVICEMODE:-660},ispeed=$BAUDRATE,ospeed=$BAUDRATE,b$BAUDRATE EXEC:"node /app/virtualavr.js $FILENAME",pty,rawer
+socat ${VERBOSITY:-} pty,rawer,link=${VIRTUALDEVICE:-'/dev/virtualavr0'},user=root,group=${DEVICEGROUP:-'dialout'},mode=${DEVICEMODE:-660},b$BAUDRATE EXEC:"node /app/virtualavr.js $FILENAME",pty,rawer
 
