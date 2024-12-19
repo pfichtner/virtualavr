d08af8b3230f5522a84942467b345834c4afa2b2
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-12-19 20:28:04 +0100

    defines for input pins

--- demo/java/sketchtest/src/test/resources/trafficlight.ino
+++ demo/java/sketchtest/src/test/resources/trafficlight.ino
@@ -1,21 +1,22 @@
-// left poti is reference, right poti is mic (actual value)
+#define REF_PIN A0
+#define VALUE_PIN A1
 
 #define RED_LED 12
 #define YELLOW_LED 11
 #define GREEN_LED 10
 
 void setup() {
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
+  pinMode(REF_PIN, INPUT);
+  pinMode(VALUE_PIN, INPUT);
   pinMode(RED_LED, OUTPUT);
   pinMode(YELLOW_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
 }
 
 void loop() {
-  int ref = analogRead(A0);
+  int ref = analogRead(REF_PIN);
   int nintyPercentOfRef = ref * 0.9;
-  int value = analogRead(A1);
+  int value = analogRead(VALUE_PIN);
 
   boolean redIsOn = value > ref;
   boolean yellowIsOn = !redIsOn && value > nintyPercentOfRef;
