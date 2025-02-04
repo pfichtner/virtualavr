5352c0bfd41ec7e374c572542707b1fe8bc0d683
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-02-04 23:46:11 +0100

    ROOTDIR replaceable for tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
+ROOTDIR="" # used to replace the ROOTDIR for tests
 
 cleanup() {
     [ -n "${PID:-}" ] && kill "$PID"
-    [ "$CLEANUP_VIRTUALDEVICE" == 'true' ] && rm -f "$VIRTUALDEVICE"
+    [ "$CLEANUP_VIRTUALDEVICE" == 'true' ] && rm -f "${ROOTDIR}${VIRTUALDEVICE}"
 }
 
+CLEANUP_VIRTUALDEVICE=false
 FILENAME=${FILENAME:-'sketch.ino'}
 BAUDRATE=${BAUDRATE:-9600}
-CLEANUP_VIRTUALDEVICE=false
 
 trap 'cleanup' EXIT
-cd /sketch
+
 
 [ -z "${VIRTUALDEVICE+x}" ] && VIRTUALDEVICE="/dev/virtualavr0"
-if [ -e "$VIRTUALDEVICE" ]; then
+
+if [ -n "$VIRTUALDEVICE" -a -e "${ROOTDIR}$VIRTUALDEVICE" ]; then
     if [ ! -v OVERWRITE_VIRTUALDEVICE ]; then
-        echo "$VIRTUALDEVICE already exists, set \$OVERWRITE_VIRTUALDEVICE if it should get overwritten" >&2
+        echo "$VIRTUALDEVICE already exists, set OVERWRITE_VIRTUALDEVICE if it should get overwritten" >&2
         exit 1
     fi
 else
-    [ -z "$VIRTUALDEVICE" ] && VIRTUALDEVICE=$(mktemp /tmp/virtualavrXXXXXXXXXX)
+    [ -z "$VIRTUALDEVICE" ] && VIRTUALDEVICE=$(mktemp "${ROOTDIR}/tmp/virtualavrXXXXXXXXXX") && VIRTUALDEVICE="${VIRTUALDEVICE#$ROOTDIR}"
     CLEANUP_VIRTUALDEVICE=true
 fi
 
-socat ${VERBOSITY:-} pty,rawer,link=${VIRTUALDEVICE},user=${DEVICEUSER:-'root'},group=${DEVICEGROUP:-'dialout'},mode=${DEVICEMODE:-660},b$BAUDRATE EXEC:"node /app/virtualavr.js $FILENAME",pty,rawer,fdin=3,fdout=4 &
+socat ${VERBOSITY:-} pty,rawer,link="${ROOTDIR}${VIRTUALDEVICE}",user=${DEVICEUSER:-'root'},group=${DEVICEGROUP:-'dialout'},mode=${DEVICEMODE:-660},b$BAUDRATE EXEC:"node /app/virtualavr.js $FILENAME",pty,rawer,fdin=3,fdout=4 &
 
 PID=$!
 wait $PID
-
